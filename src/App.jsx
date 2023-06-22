66d222940408daebe59398309df31bc77c851df9
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-22 19:01:49 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 
-const App = () => {
+function App () {
   return (
     <BrowserRouter basename="/React".react-router>
      <Router>
