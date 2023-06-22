dd273aba32eca05a4158776274e2c5e2e931f781
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-22 20:49:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,7 @@ import About from "./Pages/About";
 function App () {
   return (
     <>
-    <BrowserRouter basename="/React".react-router>
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home></Home>}/>
-        <Route path="/Contact" element={<Contact></Contact>}/>
-        <Route path="/About" element={<About></About>}/>
-      </Routes>
-    </Router>
-    </BrowserRouter>
+      <Home></Home>
     </>
   );
 }
