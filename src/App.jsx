6cc2d2b2f64f592a33b0b4902df19d85c57a5303
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-22 19:59:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-
-import Home from "./Pages/Home";
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 
